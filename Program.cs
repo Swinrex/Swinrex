cef158aec06d1bcaf8e702e2d4d04dfaca0165d5
Waleed Abu Alkhayr <80219580+Swinrex@users.noreply.github.com>
2021-07-11 16:36:43 +0300

    Add files via upload
    Variables Problem 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Variables_Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int FirstNum = 5;
+            int SecondNum = 10;
+
+            int anyvalue = FirstNum;
+            FirstNum = SecondNum;
+            SecondNum = anyvalue;
+        }
+    }
+}
